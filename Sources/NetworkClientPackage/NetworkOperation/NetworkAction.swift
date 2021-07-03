0842c6e042649298bd65d80039ca0cb0daae6932
ChandanChilamkurthy <radhachandan.chilamkurthy@prowesssoft.com>
2021-07-03 11:17:27 +0530

    Commit w.r.t changes in NetworkAction.

--- Sources/NetworkClientPackage/NetworkOperation/NetworkAction.swift
+++ Sources/NetworkClientPackage/NetworkOperation/NetworkAction.swift
@@ -3,6 +3,8 @@ import Foundation
 public final class NetworkAction: NetworkOperationProtocol {
     
     private var task: NetworkSessionTaskProtocol?
+    
+    public init() {}
 
     public func cancel() {
         task?.cancel()
